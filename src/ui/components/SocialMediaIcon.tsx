81801c405f4650e800fb249aa906b270d8efd42d
nikol.1704 <nikol.lazarte@540deg.com>
2025-02-12 23:43:00 +0100

    add social media icons

--- src/ui/components/SocialMediaIcon.tsx
+++ src/ui/components/SocialMediaIcon.tsx
@@ -1,17 +1,24 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import { Image, StyleSheet, TouchableOpacity, View } from 'react-native'
 
+const icons = {
+  facebook: require('@/assets/icons/facebook.png'),
+  twitter: require('@/assets/icons/instagram.png'),
+  instagram: require('@/assets/icons/twiter.png'),
+}
+
 export const SocialMediaIcons = ({ social }: { social: { facebook: string; twitter: string; instagram: string } }) => {
-  const icons = [
-    { name: 'facebook', url: social.facebook, icon: 'https://cdn-icons-png.flaticon.com/512/145/145802.png' },
-    { name: 'twitter', url: social.twitter, icon: 'https://cdn-icons-png.flaticon.com/512/145/145812.png' },
-    { name: 'instagram', url: social.instagram, icon: 'https://cdn-icons-png.flaticon.com/512/2111/2111463.png' },
+  const socialLinks = [
+    { name: 'facebook', url: social.facebook, icon: icons.facebook },
+    { name: 'twitter', url: social.twitter, icon: icons.twitter },
+    { name: 'instagram', url: social.instagram, icon: icons.instagram },
   ]
 
   return (
     <View style={styles.socialContainer}>
-      {icons.map(({ name, url, icon }) => (
+      {socialLinks.map(({ name, url, icon }) => (
         <TouchableOpacity key={name} onPress={() => console.log(`Opening ${name}: ${url}`)}>
-          <Image source={{ uri: icon }} style={styles.socialIcon} />
+          <Image source={icon} style={styles.socialIcon} />
         </TouchableOpacity>
       ))}
     </View>
