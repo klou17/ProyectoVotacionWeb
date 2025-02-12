847b1bc1c4a106345657145b63663a7ccbde2037
nikol.1704 <nikol.lazarte@540deg.com>
2025-02-12 23:26:42 +0100

    add fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,26 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+import * as SplashScreen from 'expo-splash-screen'
+import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
+import { useEffect } from 'react'
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    'Lato-Regular': require('../assets/fonts/Lato-Regular.ttf'),
+    'Lato-Bold': require('../assets/fonts/Lato-Bold.ttf'),
+    'Merriweather-Bold': require('../assets/fonts/Merriweather-Bold.ttf'),
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded, error])
+
+  if (!loaded && !error) {
+    return null
+  }
+
   return (
     <Stack>
       <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
